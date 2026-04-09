4179f120543496083ad0bce81933070c36be6baa
MahammadGaleeb <mahammadgaleeb@gmail.com>
2026-04-09 08:39:02 +0530

    09-04-2026

---
+++ lab-12/exercise-1/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Import controller
+const userController = require('../controllers/userController');
+
+// Define routes
+router.get('/', userController.getUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
